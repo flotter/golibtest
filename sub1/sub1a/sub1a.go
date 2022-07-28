4040e82c3b458928e0dcab3db0ed26c8580a9d0d
Fred Lotter <fred.lotter@canonical.com>
2022-07-28 17:05:04 +0200

    Complex

--- sub1/sub1a/sub1a.go
+++ sub1/sub1a/sub1a.go
@@ -2,14 +2,22 @@ package sub1a
 
 import "fmt"
 
+func PublicGeneric(y int) int {
+	return y + 1
+}
+
+func privateGeneric(y int) int {
+	return y + 10
+}
+
 func Sub1a_1(x int) int {
-	res := x * x
+	res := x * x + PublicGeneric(x) + privateGeneric(x)
 	fmt.Println("sub1a_1 says :", res)
 	return res
 }
 
 func Sub1a_2(x int) int {
-	res := x * x
+	res := x * x + PublicGeneric(x) + privateGeneric(x)
 	fmt.Println("sub1a_2 says :", res)
 	return res
 }
